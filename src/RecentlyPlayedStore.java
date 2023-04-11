32bd8cc0197bd0d3eae116dcdf1526bd786b8be9
ABHIJIT4564 <abhijitchorage93@gmail.com>
2023-04-11 18:07:45 +0530

    change
    changed 4 to 3

---
+++ src/RecentlyPlayedStore.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class RecentlyPlayedStore {
+    private int capacity;
+    private int numSongsPerUser;
+    private Map<String, String> songUserMap; // song -> user
+    private Map<String, LinkedList<String>> userSongListMap; // user -> song list
+
+    public RecentlyPlayedStore(int capacity, int numSongsPerUser) {
+        this.capacity = capacity;
+        this.numSongsPerUser = numSongsPerUser;
+        songUserMap = new HashMap<>();
+        userSongListMap = new HashMap<>();
+    }
+
+    public List<String> getRecentlyPlayedSongs(String user) {
+        List<String> songList = userSongListMap.get(user);
+        if (songList == null) {
+            return new ArrayList<>();
+        } else {
+            return new ArrayList<>(songList);
+        }
+    }
+
+    public void addSongForUser(String user, String song) {
+        LinkedList<String> songList = userSongListMap.get(user);
+        if (songList == null) {
+            songList = new LinkedList<>();
+            userSongListMap.put(user, songList);
+        }
+        if (songUserMap.containsKey(song)) {
+            String prevUser = songUserMap.get(song);
+            userSongListMap.get(prevUser).remove(song);
+        }
+        songUserMap.put(song, user);
+        songList.add(song);
+
+        if (songList.size() > numSongsPerUser) {
+            String oldestSong = songList.removeFirst();
+            songUserMap.remove(oldestSong);
+        }
+        if (songUserMap.size() > capacity) {
+            String oldestSong = userSongListMap.get(songUserMap.entrySet().iterator().next().getValue()).removeFirst();
+            songUserMap.remove(oldestSong);
+        }
+    }
+  public void main(String[] args){
+      RecentlyPlayedStore obj= new RecentlyPlayedStore(3,4);
+      obj.getRecentlyPlayedSongs("Abhijit");
+      obj.addSongForUser("Abhijit","S1");
+      obj.addSongForUser("Abhijit","S2");
+      obj.addSongForUser("Abhijit","S3");
+      obj.addSongForUser("Abhijit","S4");
+     System.out.println(userSongListMap.containsValue(3));
+  }
+}
+
+
